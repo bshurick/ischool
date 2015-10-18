7d7112ed922deb33649d2beb0d014aff70ab8fd8
Brandon Shurick <bshurick@berkeley.edu>
2015-10-18 20:28:03 +0000

    add survey transform

---
+++ Data Storage/exercise_1/transforming/survey.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS production.Survey;
+CREATE TABLE production.Survey
+STORED AS ORC 
+AS
+SELECT 
+provider_number as provider_id,
+communication_with_nurses_achievement_points ,
+communication_with_nurses_improvement_points ,
+communication_with_nurses_dimension_score ,
+communication_with_doctors_achievement_points ,
+communication_with_doctors_improvement_points ,
+communication_with_doctors_dimension_score ,
+responsiveness_of_hospital_staff_achievement_points ,
+responsiveness_of_hospital_staff_improvement_points ,
+responsiveness_of_hospital_staff_dimension_score ,
+pain_management_achievement_points ,
+pain_management_improvement_points ,
+pain_management_dimension_score ,
+communication_about_medicines_achievement_points ,
+communication_about_medicines_improvement_points ,
+communication_about_medicines_dimension_score ,
+cleanliness_and_quietness_of_hospital_environment_achievement_points ,
+cleanliness_and_quietness_of_hospital_environment_improvement_points ,
+cleanliness_and_quietness_of_hospital_environment_dimension_score ,
+discharge_information_achievement_points ,
+discharge_information_improvement_points ,
+discharge_information_dimension_score ,
+overall_rating_of_hospital_achievement_points ,
+overall_rating_of_hospital_improvement_points ,
+overall_rating_of_hospital_dimension_score ,
+hcahps_base_score ,
+hcahps_consistency_score
+FROM staging.surveys_responses
+SORT BY provider_id
+;
