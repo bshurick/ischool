2ca6c13840ebb7b29bd56b9a52c51377e95d71d5
Brandon Shurick <bshurick@berkeley.edu>
2015-10-18 22:12:26 +0000

    add best hospital calculation and results

--- Data Storage/exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ Data Storage/exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -1,12 +1,23 @@
+USE production;
+SET hive.cli.print.header=true;
 SELECT
+
 h.hospital_name
-, sum(p.score) total_score
-, avg(p.score) avg_score 
-, stddev_pop(p.score) stdev_score 
-from production.Hospital h 
-	left join production.Procedure p 
-	on h.provider_id = p.provider_id 
+,avg(measure_rank) avg_measure_rank
+,stddev(measure_rank) stddev_measure_rank 
+,sum(measure_rank) sum_measure_rank
+,count(measure_rank) cnt_measure_rank
+
+from (
+select 
+provider_id
+, percent_rank() OVER (partition by measure_id order by score) measure_rank 
+from Procedure
+) m
+join Hospital h
+on m.provider_id = h.provider_id 
+
 group by h.hospital_name
-order by avg_score desc 
+order by avg_measure_rank desc 
 limit 10
 ;
