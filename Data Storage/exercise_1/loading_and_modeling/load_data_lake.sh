423fe5f766fc4acb187ae2c5fa165bc2033e13a9
w205 <w205@ip-10-236-181-152.ec2.internal>
2015-10-18 18:12:25 +0000

    updated load data script + ddl statements

--- Data Storage/exercise_1/loading_and_modeling/load_data_lake.sh
+++ Data Storage/exercise_1/loading_and_modeling/load_data_lake.sh
@@ -1,14 +1,24 @@
+#!/usr/bin/env bash
+
+# Remove old files
+hadoop fs -rm -r -f /user/w205/hospital_compare
+hadoop fs -mkdir /user/w205/hospital_compare
+hadoop fs -mkdir /user/w205/hospital_compare/hospitals
+hadoop fs -mkdir /user/w205/hospital_compare/effective_care
+hadoop fs -mkdir /user/w205/hospital_compare/readmissions
+hadoop fs -mkdir /user/w205/hospital_compare/measure_dates
+hadoop fs -mkdir /user/w205/hospital_compare/surveys_responses
 
 # Rename files
-mv 'Hospital General Information.csv' hospitals.csv
-mv 'Timely and Effective Care - Hospital.csv' effective_care.csv
-mv 'Readmissions and Deaths - Hospital.csv' readmissions.csv
-mv 'Measure Dates.csv' measuredates.csv
-mv hvbp_hcahps_05_28_2015.csv surveys_responses.csv
+cat 'Hospital General Information.csv' | awk 'NR>1' > hospitals.csv
+cat 'Timely and Effective Care - Hospital.csv' | awk 'NR>1' > effective_care.csv
+cat 'Readmissions and Deaths - Hospital.csv' | awk 'NR>1' > readmissions.csv
+cat 'Measure Dates.csv' | awk 'NR>1' > measure_dates.csv
+cat hvbp_hcahps_05_28_2015.csv | awk 'NR>1' > surveys_responses.csv
 
 # Send files to HDFS
-hadoop fs -copyFromLocal hospitals.csv /user/w205/hospital_compare/hospitals.csv
-hadoop fs -copyFromLocal effective_care.csv /user/w205/hospital_compare/effective_care.csv
-hadoop fs -copyFromLocal readmissions.csv /user/w205/hospital_compare/readmissions.csv
-hadoop fs -copyFromLocal measuredates.csv /user/w205/hospital_compare/measuredates.csv
-hadoop fs -copyFromLocal surveys_responses.csv /user/w205/hospital_compare/surveys_responses.csv
+hadoop fs -copyFromLocal hospitals.csv /user/w205/hospital_compare/hospitals/hospitals.csv
+hadoop fs -copyFromLocal effective_care.csv /user/w205/hospital_compare/effective_care/effective_care.csv
+hadoop fs -copyFromLocal readmissions.csv /user/w205/hospital_compare/readmissions/readmissions.csv
+hadoop fs -copyFromLocal measure_dates.csv /user/w205/hospital_compare/measure_dates/measure_dates.csv
+hadoop fs -copyFromLocal surveys_responses.csv /user/w205/hospital_compare/surveys_responses/surveys_responses.csv
