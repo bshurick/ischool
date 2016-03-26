f6673cba37f4b939726296d5b9c44ebc97e000e3
bshur2008 <bshur2008@gmail.com>
2016-03-26 16:23:09 +0100

    add responsive video

--- Data Visualization/Project/morality/main/views.py
+++ Data Visualization/Project/morality/main/views.py
@@ -7,13 +7,8 @@ from geodata.views import get_geodata, get_fillkey
 
 def index(request):
 	videos = {
-		'test':'''
-                                <iframe width="600" height="315" src="https://www.youtube.com/embed/2DN0IRoMf4k" frameborder="0" allowfullscreen></iframe>
-                        '''
-		,'control':'''
-                                <iframe width="600" height="315" src="https://www.youtube.com/embed/V6-0kYhqoRo" frameborder="0" allowfullscreen></iframe>
-                        '''
-
+		'test':'https://www.youtube.com/embed/2DN0IRoMf4k'
+		,'control':'https://www.youtube.com/embed/V6-0kYhqoRo'
 	}
 	if not request.session.get('video'):
 		r = int(random.random()*10000)
