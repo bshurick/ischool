9ace6fe5255284e597c6959da94a3636c79e899c
bshur2008 <bshur2008@gmail.com>
2016-05-23 19:12:59 +0200

    add question 8

--- Time Series Analysis/lab1.R
+++ Time Series Analysis/lab1.R
@@ -7,3 +7,19 @@ x <- 1-pbinom(1, 12, 0.2)
 print(paste0('Probability of 2+ jurors: ',round(x*100,2),'%'))
 
 
+# Question 8
+players <- c('Mark Price','Trent Tucker','Dale Ellis','Craig Hodges'
+            ,'Danny Ainge','Byron Scott','Reggie Miller','Larry Bird'
+            ,'Jon Sundvold','Brian Taylor')
+fga <- c(429,833,1149,1016,1051,676,416,1206,440,417)
+fgm <- c(188,345,472,396,406,260,159,455,166,157)
+theta <- fgm/fga
+Y <- mean(theta)
+MP <- theta[1]
+n <- sum(fga)
+seY <- sqrt(Y*(1-Y)/n)
+Z <- (Y-MP)/seY
+p_value <- pnorm(Z)
+print(paste0('SD of theta: ',round(sd(theta),6)))
+print(paste0('SD of FGA: ',round(sd(fga),2)))
+print(paste0('P-value: ',round(p_value,16)))
