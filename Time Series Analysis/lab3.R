dfec6d9a4830b3fc4e697a06cfcdbbe8f534aa28
bshur2008 <bshur2008@gmail.com>
2016-08-06 22:40:10 -0700

    add part 2

--- Time Series Analysis/lab3.R
+++ Time Series Analysis/lab3.R
@@ -62,10 +62,10 @@ legend("topleft", legend=leg.txt, lty=c(2,1), col=c("navy","blue"),
        bty='n', cex=1)
 
 # plot ACF and PCF
-acf(fp.diff, lag.max=120, 
-        main='ACF of First Difference')
-pacf(fp.diff, lag.max=120, 
-        main='PACF of First Difference')
+acf(fp, lag.max=120, 
+        main='ACF')
+pacf(fp, lag.max=120, 
+        main='PACF')
 
 #####################################################################
 
@@ -74,7 +74,7 @@ pacf(fp.diff, lag.max=120,
 ## Fit Arima Model
 
 # arima model 
-get.best.arima <- function(x.ts, test.ts, method='AIC', maxord = c(1,1,1,1,1,1))
+get.best.arima <- function(x.ts, test.ts=NA, method='AIC', maxord = c(1,1,1,1,1,1))
 {
   best.aic <- 1e8
   best.rmse <- 1e8
@@ -88,10 +88,10 @@ get.best.arima <- function(x.ts, test.ts, method='AIC', maxord = c(1,1,1,1,1,1))
                    seas = list(order = c(P,D,Q),
                                frequency(x.ts)), method = "CSS")
       fit.aic <- -2 * fit$loglik + (log(n) + 1) * length(fit$coef)
-      fit.fcast <- forecast.Arima(fit, h=H)
-      fit.rmse <- sqrt(mean((fit.fcast$mean-test.ts)**2))
       if (method=='RMSE')
       {
+        fit.fcast <- forecast.Arima(fit, h=H)
+        fit.rmse <- sqrt(mean((fit.fcast$mean-test.ts)**2))
         if (fit.rmse < best.rmse)
         {
           best.rmse <- fit.rmse
@@ -104,7 +104,7 @@ get.best.arima <- function(x.ts, test.ts, method='AIC', maxord = c(1,1,1,1,1,1))
       {
         if (fit.aic < best.aic)
         {
-          best.rmse <- fit.rmse
+          best.rmse <- NA
           best.aic <- fit.aic
           best.fit <- fit
           best.model <- c(p,d,q,P,D,Q)
@@ -115,12 +115,16 @@ get.best.arima <- function(x.ts, test.ts, method='AIC', maxord = c(1,1,1,1,1,1))
   list(best.rmse, best.aic, best.fit, best.model)
 }
 N <- length(fp)
-test <- (N - 52):(N)
-train <- 1:(N - 53)
+test <- (N - 51):(N)
+train <- 1:(N - 52)
 get.best.arima(fp[train], fp[test], method='RMSE')
 
 # fit best model
-fp.best_arima <- arima(x = fp, order=c(0,0,1), seasonal=list(order=c(0,0,1)))
+fp.best_arima <- arima(x = fp, 
+                       order=c(0,0,1), 
+                       seasonal=list(order=c(0,0,1),  
+                                     frequency(fp)),
+                       method = "CSS")
 
 #####################################################################
 
@@ -142,10 +146,6 @@ summary(fp.best_arima$residuals)
 # make forecast 
 fp.best_arima.fcast <- forecast.Arima(fp.best_arima, h=52)
 
-# RMSE 
-rmse <- sqrt(mean((fp.best_arima.fcast$mean-fp[test][1:52])**2))
-print(paste0('RMSE: ',round(rmse,4)))
-
 # Plot forecast vs original
 dev.off()
 par(mfrow=c(1,1))
@@ -159,6 +159,170 @@ plot.ts(fitted(fp.best_arima.fcast),
         col="blue",lty=1,axes=F, xlim=xlimits,ylim=ylimits,ylab='')
 par(new=T)
 plot.ts(fp, col="navy",axes=F,xlim=xlimits,ylim=ylimits,ylab="", lty=2)
+
+# add legend
+leg.txt <- c("Original Series", "Fitted series", "Forecast")
+legend("topleft", legend=leg.txt, lty=c(2,1,1),
+       col=c("navy","blue","blue"), lwd=c(1,1,2),
+       bty='n', cex=1)
+
+#####################################################################
+
+
+#####################################################################
+#########                      PART 2                       #########
+#####################################################################
+
+
+#####################################################################
+## Setup
+
+# Load libraries
+require(tseries)
+
+# Read dataset 
+load('/Users/bshur/School/Time Series Analysis/lab3/gasOil.Rdata')
+str(gasOil)
+
+#####################################################################
+
+
+####################################################################
+# Evaluate Dataset
+
+# Take summary statistics
+summary(gasOil)
+
+# Create time-series objects
+gasOil.Production.ts <- ts(gasOil$Production, start=c(1978, 1), frequency=12)
+gasOil.Price.ts <- ts(gasOil$Price, start=c(1978, 1), frequency=12)
+
+####################################################################
+
+
+####################################################################
+# Reproduce Correlation Results
+
+# Model Price~Production
+plot(gasOil[, c('Production','Price')])
+m <- lm(Price~Production, data=gasOil)
+summary(m)
+
+####################################################################
+
+
+####################################################################
+# Evaluate Dataset
+
+# Plot Time Series
+dev.off()
+par(mfrow=c(2,1))
+plot.ts(gasOil.Price.ts, main='')
+plot.ts(gasOil.Production.ts)
+
+# Evaluate Correlograms
+dev.off()
+par(mfrow=c(2,1))
+acf(gasOil.Price.ts)
+acf(gasOil.Production.ts)
+
+# Test for unit roots
+adf.test(gasOil.Price.ts)
+adf.test(gasOil.Production.ts)
+pp.test(gasOil.Price.ts)
+pp.test(gasOil.Production.ts)
+
+# Test for co-integration
+po.test(cbind(gasOil.Price.ts, gasOil.Production.ts))
+
+# Fails to reject null, go with univariate model
+dev.off()
+par(mfrow=c(2,2))
+plot.ts(gasOil.Price.ts, main='Gas Prices', lty=2, col='navy', ylab='Gas Prices')
+lines(filter(gasOil.Price.ts, rep(1,12)/12, sides=2), 
+      main='Gas Prices, 12-month Moving Average',
+      ylab='Gas Prices',
+      xlab='Month',
+      col="blue")
+
+# add legend
+leg.txt <- c("Original Series", "Moving Average")
+legend("topleft", legend=leg.txt, lty=c(2,1), col=c("navy","blue"),
+       bty='n', cex=1)
+
+# experiment with differential
+gasOil.Price.ts.diff <- diff(gasOil.Price.ts, lag=1)
+plot.ts(gasOil.Price.ts.diff, 
+        main='First Difference of Gas Prices', 
+        lty=2,
+        ylab='Gas Price Differential') 
+lines(filter(gasOil.Price.ts.diff, rep(1,12)/12, sides=2), 
+      main='Gas Prices, 12-month Moving Average',
+      ylab='Gas Prices',
+      xlab='Month',
+      col='blue')
+
+# add legend
+leg.txt <- c("Original Series", "Moving Average")
+legend("topleft", legend=leg.txt, lty=c(2,1), col=c("navy","blue"),
+       bty='n', cex=1)
+
+# plot ACF and PCF
+acf(gasOil.Price.ts, lag.max=120, 
+    main='ACF')
+pacf(gasOil.Price.ts, lag.max=120, 
+     main='PACF')
+
+#####################################################################
+
+
+#####################################################################
+## Fit Arima Model
+
+# Fit model using AIC 
+get.best.arima(gasOil.Price.ts)
+
+# fit best model
+gasOil.Price.best_arima <- arima(x = gasOil.Price.ts, 
+                                 order=c(0,1,1), 
+                                 seasonal=list(order=c(1,0,1),  
+                                               frequency(gasOil.Price.ts)),
+                                 method = "CSS")
+
+#####################################################################
+
+
+#####################################################################
+## Evaluate ARIMA Model
+
+# plot model in-sample residuals
+dev.off()
+par(mfrow=c(2,2))
+plot(gasOil.Price.best_arima$residuals, main='ARIMA (0,1,1)(1,0,1) In-sample Residuals')
+hist(gasOil.Price.best_arima$residuals, main='ARIMA (0,1,1)(1,0,1) In-sample Residuals')
+acf(gasOil.Price.best_arima$residuals, main='ACF: ARIMA (0,1,1)(1,0,1) In-sample Residuals')
+pacf(gasOil.Price.best_arima$residuals, main='PACF: ARIMA (0,1,1)(1,0,1) In-sample Residuals')
+
+# summary 
+summary(gasOil.Price.best_arima$residuals)
+
+# make forecast 
+gasOil.Price.best_arima.fcast <- forecast.Arima(gasOil.Price.best_arima, h=12*4-2)
+
+# Plot forecast vs original
+dev.off()
+par(mfrow=c(1,1))
+xlimits <- c(1978, 2016)
+ylimits <- c(0, 7.5)
+plot(gasOil.Price.best_arima.fcast, lty=2, 
+     xlim=xlimits, ylim=ylimits,
+     main="Out-of-Sample Forecast",
+     ylab="Original, Estimated, and Forecast Values")
+par(new=T)
+plot.ts(fitted(gasOil.Price.best_arima.fcast), 
+        col="blue",lty=1,axes=F, xlim=xlimits,ylim=ylimits,ylab='')
+par(new=T)
+plot.ts(gasOil.Price.ts,col="navy",axes=F,xlim=xlimits,ylim=ylimits,ylab="", lty=2)
 
 # add legend
 leg.txt <- c("Original Series", "Fitted series", "Forecast")
